56549b1300a7c38614b4c466d5b6805e4cc5c81a
Pasindya Chavindee <151112355+Pasindya@users.noreply.github.com>
2024-09-10 01:24:49 +0530

    created DB connection

--- backend/app.js
+++ backend/app.js
@@ -1,1 +1,18 @@
-console.log("bye");
+
+const express = require("express");
+const mongoose = require("mongoose");
+
+const app = express();
+
+
+//Middleware
+app.use("/",(req, res, next) => {
+    res.send("Hi thisura");
+})
+
+mongoose.connect("mongodb+srv://surfdeck:surfdeck1234@cluster0.kcpia.mongodb.net/")
+.then(() => console.log("Connected to MongoDB"))
+.then(() => {
+    app.listen(5000);
+})
+.catch((err)=> console.log((err)));
